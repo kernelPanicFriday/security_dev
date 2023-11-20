bce3844a3475ddad835d7e102fc21f2303a683fe
kernelPanicFriday <tyler.evrhett@gmail.com>
2023-11-20 14:48:39 -0500

    Initial, working code

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware 
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/shit")
+async def root():
+    print('sending')
+    return "hello world"
